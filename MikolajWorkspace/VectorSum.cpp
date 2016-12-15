c01b881f3be1257d8154c1543af41bab2c6b9a99
YOGA-ARKADIUSZ\Arkadiusz <zimniutko@gmail.com>
2016-12-15 20:24:09 +0100

    Juz nie powinna zwracac pustego vectora

--- MikolajWorkspace/VectorSum.cpp
+++ MikolajWorkspace/VectorSum.cpp
@@ -6,44 +6,50 @@
 
 using namespace std;
 
-vector<int> sum(const vector<int> &v1, const vector<int> &v2) {
-	vector<int> v;
-	if (v1.size() != v2.size()) {
+void sum(const vector<int> &v1, const vector<int> &v2, vector<int>& v)
+{
+	if (v1.size() != v2.size())
+	{
 		cout << "Wektory sa roznych rozmiarow" << endl;
-		return v; // nie podoba mi siê to rozwiazanie, wolalbym nic nie zwracac niz pusty wektor
+		// nie podoba mi siê to rozwiazanie, wolalbym nic nie zwracac niz pusty wektor
+		//-> Teraz funkcja odrazu koñczy wykonywanie
 	}
 	else {
 		int range = max(v1.size(), v2.size());
 		v.resize(range);
-		for (int i = 0; i < range; i++) { //nie wiem czy da sie to zrobic z iteratorem
+		for (int i = 0; i < range; i++) // -> Raczej ciêzko o to, iterator jest ekluzywny dla kontenera poprzez to vector.begin() i vector.end()
+		{
 			v[i] = v1[i] + v2[i];
 		}
-		return v;
 	}
 }
-
-int main() {
+int main()
+{
 
 	vector<int> v1;
 	vector<int> v2;
 	vector<int> v;
-	
+
 	v1.resize(5);
 	v2.resize(5);
-	
-	for (auto itr = v1.begin(); itr != v1.end();itr++) {
+
+	cout << "Podaj elementy pierwszego wektora" << endl;
+	for (auto itr = v1.begin(); itr != v1.end(); itr++) {
+
 		int x;
 		cin >> x;
 		*itr = x;
 	}
-	for (auto itr = v2.begin(); itr != v2.end();itr++) {
+	cout << "Podaj elementy drugiego wektora" << endl;
+	for (auto itr = v2.begin(); itr != v2.end(); itr++) {
+
 		int x;
 		cin >> x;
 		*itr = x;
 	}
-	
-	v = sum(v1, v2);
-	for (auto itr = v.begin(); itr != v.end();itr++) {
+
+	sum(v1, v2, v);
+	for (auto itr = v.begin(); itr != v.end(); itr++) {
 		cout << *itr << endl;
 	}
 
