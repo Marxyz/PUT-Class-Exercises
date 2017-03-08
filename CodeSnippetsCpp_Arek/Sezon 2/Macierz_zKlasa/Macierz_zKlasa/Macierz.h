236b39f14341bbae14454ee91dc514fdee216b2d
DESKTOP-UBADV2C\User <zimniutko@gmail.com>
2017-03-08 16:06:50 +0100

    Szkielet zadania z macierza, dodanie porządnego gitignora.

---
+++ CodeSnippetsCpp_Arek/Sezon 2/Macierz_zKlasa/Macierz_zKlasa/Macierz.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Macierz
+{
+	int _iloscKolumn;
+	int _iloscRzedow;
+	vector<vector<int>> _zasob;
+
+	Macierz DodajMacierz(Macierz macierzB);
+	Macierz PomnozMacierz(Macierz macierzB);
+	void PobierzMacierz();
+	void Wyswietl();
+	
+
+public:
+
+	Macierz();
+	Macierz(int wysokosc, int szerokosc);
+
+};
