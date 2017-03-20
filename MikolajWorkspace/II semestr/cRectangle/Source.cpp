f82c40d90705b1ab662ac324c6d0455d3231063a
kolaszko <mikolaj.lysakowski.bk@gmail.com>
2017-03-20 01:18:55 +0100

    final src

--- MikolajWorkspace/II semestr/cRectangle/Source.cpp
+++ MikolajWorkspace/II semestr/cRectangle/Source.cpp
@@ -30,13 +30,13 @@ void resize(int width, int height)
 	glLoadIdentity();
 }
 
-void idle() //kiedy nie ma co robic xd; obczaic gluttimerfunc
+void idle() 
 {
 	glutPostRedisplay();
 }
 
 void Keyboard(unsigned char key, int mouse_x, int mouse_y) {
-	//std::cout << "Naciœniêto klawisz: " << key << ", a myszka znajduje siê w pozycji: " << mouse_x << ", " << mouse_y << "(w pikselach)" << std::endl;
+
 	switch (key) {
 	case 'a': {
 		if (obiekty.size() > 0) obiekty[active].Move(-0.01, 0.0);
@@ -62,8 +62,15 @@ void Keyboard(unsigned char key, int mouse_x, int mouse_y) {
 	}
 	case 'e': {
 		if (obiekty.size() > 0) {
-			obiekty.erase(obiekty.begin() + active);
-			active--;
+			int wsk = 0;
+			for (auto itr = obiekty.begin(); itr != obiekty.end(); itr++) {
+				if (wsk == active) {
+					obiekty.erase(itr);
+					break;
+				}
+				wsk++;
+			}
+			active=obiekty.size()-1;
 		}
 		break;
 	}
@@ -84,9 +91,8 @@ void Keyboard(unsigned char key, int mouse_x, int mouse_y) {
 }
 
 
-void display() //wywolywana kiedy okno wymaga przerysowania
+void display() 
 {
-	// clear the scene
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glPushMatrix();
@@ -102,7 +108,7 @@ void InitGLUTScene(char* window_name)
 {
 	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(40, 40);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE); //GLUT_DOUBLE - double buffer, glut_depth - 
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE); 
 
 	glutCreateWindow(window_name);
 
@@ -132,23 +138,10 @@ int main(int argc, char *argv[])
 	
 	SetCallbackFunctions();
 
-	// start GLUT event loop. It ends when user close the window.
+	
 	glutMainLoop();
 
 	return 0;
 }
 
 
-/* Napisac cRectangle, z polami dlugosc, wysokosc, polozenie srodka x,y, alfa do obrotu, R G B; z metodami: dwa konstruktory, przesun_o(dx,dy), przesun_do(x,y)
-rysuj(), obroc(dalfa), setcolor(r,g,b), resize(w,a)
-
-
-globalny obiekt rectangle + golablny vector<cRectangle> tab;
-zmienna globalna int currentID;
-'n' = dodaj nowy prostokat
-'e' = usun prostokat
-'+' - zwiekszanie current o 1;
-'-' zmniejszanie current o 1
-
-tab[currentID]. move - przesuneicie aktywnego obiektu
--*/
