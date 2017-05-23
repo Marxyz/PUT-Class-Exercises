305c90898255d403209f82411ea9ede1945a7248
DESKTOP-UBADV2C\User <zimniutko@gmail.com>
2017-05-23 16:24:39 +0200

    Init

---
+++ UR3CPP/Ur3ToCpp/Ur3ToCpp/UR3Intermediator.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include "UR3Message.h"
+#include <string>
+using namespace std;
+class UR3Intermediator
+{
+
+public:
+
+	bool Connect();
+	vector<int> ReadDataFlow();
+	bool SendData(string Method);
+
+	void LogData(string data);
+	void ViewData();
+
+	volatile vector<int> DataFlow;
+
+
+	UR3Message DecipherMessage(const vector<int>& DataFlow);
+
+
+
+};
+
---
+++ UR3CPP/Ur3ToCpp/Ur3ToCpp/UR3Message.h
@@ -0,0 +1,39 @@
+#pragma once
+
+
+enum MessageType
+{
+	ROBOT_STATE = 16, ROBOT_MESSAGE = 20, PROGRAM_STATE_MESSAGE = 25
+};
+
+enum PackageTypes
+{
+	ROBOT_MODE_DATA = 0,
+	JOINT_DATA = 1,
+	TOOL_DATA = 2,
+	MASTERBOARD_DATA = 3,
+	CARTESIAN_INFO = 4,
+	KINEMATICS_INFO = 5,
+	CONFIGURATION_DATA = 6,
+	FORCE_MODE_DATA = 7,
+	ADDITIONAL_INFO = 8,
+	CALIBRATION_DATA = 9,
+	SAFETY_DATA = 10
+};
+
+
+
+
+class UR3Message
+{
+public:
+
+	int MessageSize;
+
+
+
+
+	UR3Message();
+	~UR3Message();
+};
+
