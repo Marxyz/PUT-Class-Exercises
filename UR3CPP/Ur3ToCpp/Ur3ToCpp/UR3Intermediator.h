b66a04cce4f6602090ff4cb6d7532daed6960f73
DESKTOP-UBADV2C\User <zimniutko@gmail.com>
2017-05-23 16:59:58 +0200

    DodanieMessageOut(wiadomosc wychodzaca)

--- UR3CPP/Ur3ToCpp/Ur3ToCpp/UR3Intermediator.h
+++ UR3CPP/Ur3ToCpp/Ur3ToCpp/UR3Intermediator.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include "UR3Message.h"
 #include <string>
+class UR3MessageOut;
 using namespace std;
 class UR3Intermediator
 {
@@ -9,16 +10,20 @@ class UR3Intermediator
 public:
 
 	bool Connect();
+
 	vector<int> ReadDataFlow();
-	bool SendData(string Method);
+	
+	bool SendData(string ParsedToStringMethod);
+	string ParseOutput(UR3MessageOut MessageToSend);
 
 	void LogData(string data);
 	void ViewData();
+	UR3Message DecipherMessage(const vector<int>& DataFlow);
 
 	volatile vector<int> DataFlow;
 
 
-	UR3Message DecipherMessage(const vector<int>& DataFlow);
+	
 
 
 
