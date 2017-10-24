7df1784610c656db2c94c99587424da9b078ae12
kolaszko <mikolaj.lysakowski.bk@gmail.com>
2017-10-24 02:01:27 +0200

    Update histogramy.m

--- histogramy.m
+++ histogramy.m
@@ -2,17 +2,17 @@ clc;
 clear all;
 close all;
 
-%data = -4.2 + 2.3*randn(1,10);
+data = -4.2 + 2.3*randn(1,100000);
 %data = [-5 4 2 1 1 3 8 -2 -3 5];
 %data = 9 + 2.3*randn(1,10);
-data = [-1 2 4 -3 5 7 -3 2 2 -1];
+%data = [-1 2 4 -3 5 7 -3 2 2 -1];
 wartOczek = WartOczekiwana(data);
 odchylenie = OdchylenieStandardowe(data);
 
 maxValue = max(data);
 minValue = min(data);
 
-bins = 10;
+bins = 100;
 wbin = (maxValue-minValue)/bins;
 bars = zeros(1,bins);
 
@@ -23,8 +23,17 @@ for i=1:length(data)
     if ind > bins
         ind = ind -1;
     end;
-    bars(ind) = bars(ind)+1;  
+    bars(ind) = bars(ind)+1 ; 
 end
 
 x = minValue:wbin:maxValue-wbin;
+for i=1:bins
+    value = bars(i);
+    bars(i) = value/(length(data)*wbin);
+end
+
 barh(x,bars);
+hold on;
+plot(normpdf2(x,-4.2,2.3),x);
+
+
